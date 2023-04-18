3193579b61e3013b96c0bb80b0a6272cae9bfa1b
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-18 18:43:24 +0430

    Add-PostAdmin

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from Blog.models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "publish", "status"]
+    list_editable = ["status"]
+    list_filter = ["status", "publish", "created", "author"]
+    ordering = ["title", "publish"]
+    search_fields = ["title", "description"]
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    prepopulated_fields = {
+        "slug": ["title"]
+    }
