289304799c5a28b68afcb680a2ad2fced162b4b4
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-18 18:34:45 +0430

    Add-PostModel

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
 
+
 # Create your models here.
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+        REJECTED = "RJ", "Rejected"
+
+    # Relations
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    # Data fields
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    slug = models.SlugField(max_length=250)
+    # Date fields
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # Choice fields
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"])
+        ]
+
+    def __str__(self):
+        return self.title
